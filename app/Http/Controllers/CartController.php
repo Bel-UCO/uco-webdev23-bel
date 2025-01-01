2643b81af4168d099503c31c59c20994e7490bc0
Bel-UCO <bpoetridhanti@student.ciputra.ac.id>
2025-01-02 01:39:45 +0700

    mulai cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    //
-}
+    public function index() {
+        $userId = Auth::id();
+        $cartItems = Cart::where('user_id', $userId)->with('product')->get();
+
+        $showFilters = false;
+
+        return view('cart.index', compact('cartItems', 'showFilters'));
+    }
+};
